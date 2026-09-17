93cd5662e51d6b4e369300a20ce6e6e196c69a2d
代码搬运工 <994301536@qq.com>
2026-09-17 09:33:34 +0800

    增加对localStorage1和sessionStorage的支持

---
+++ src/window/cache.ts
@@ -0,0 +1,67 @@
+import { postMessage } from "./index"
+
+export const localStorage1 = {
+  get(key: string): Promise<string> {
+    return postMessage({
+      funName: 'localStorage.getItem',
+      arguments: arguments,
+    })
+  },
+  key(index: number): Promise<string|null> {
+    return postMessage({
+      funName: 'localStorage.key',
+      arguments: arguments,
+    })
+  },
+  remove(key: string): Promise<void> {
+    return postMessage({
+      funName: 'localStorage.removeItem',
+      arguments: arguments,
+    })
+  },
+  set(key: string, value: string): Promise<void> {
+    return postMessage({
+      funName: 'localStorage.setItem',
+      arguments: arguments,
+    })
+  },
+  clear(): Promise<void> {
+    return postMessage({
+      funName: 'localStorage.clear',
+      arguments: arguments,
+    })
+  }
+}
+
+export const sessionStorage = {
+  get(key: string): Promise<string|null> {
+    return postMessage({
+      funName: 'sessionStorage.getItem',
+      arguments: arguments,
+    })
+  },
+  key(index: number): Promise<string|null> {
+    return postMessage({
+      funName: 'sessionStorage.key',
+      arguments: arguments,
+    })
+  },
+  remove(key: string): Promise<void> {
+    return postMessage({
+      funName: 'sessionStorage.removeItem',
+      arguments: arguments,
+    })
+  },
+  set(key: string, value: string): Promise<void> {
+    return postMessage({
+      funName: 'sessionStorage.setItem',
+      arguments: arguments,
+    })
+  },
+  clear(): Promise<void> {
+    return postMessage({
+      funName: 'sessionStorage.clear',
+      arguments: arguments,
+    })
+  }
+}
